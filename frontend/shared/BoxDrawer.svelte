ff52d351053fcd882f3f64138684961cf004ad7a
asutermo <a131313.com>
2024-03-29 12:03:23 -0600

    change bbox to black and ref transparent fill

--- frontend/shared/BoxDrawer.svelte
+++ frontend/shared/BoxDrawer.svelte
@@ -160,7 +160,8 @@
 
   function draw_boxes(boxes: Array<Array<number>>) {
     if (!ctx) return;
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.strokeStyle = "rgb(0, 0, 0)"; 
+    ctx.fillStyle = "rgba(255, 0, 0, 0.1)";
     ctx.beginPath();
     boxes.forEach((box: Array<number>) => {
       if (box[0] != box[2] && box[1] != box[3]) {
