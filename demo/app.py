9f0f231b56d19a09cb9cf93a878b4e5865aeca51
asutermo <a131313.com>
2024-03-29 16:00:03 -0600

    update

--- demo/app.py
+++ demo/app.py
@@ -4,6 +4,6 @@ from gradio_image_prompter import ImagePrompter
 demo = gr.Interface(
     lambda prompts: (prompts["image"], prompts["bbox"]),
     ImagePrompter(show_label=False),
-    [gr.Image(show_label=False), gr.Dataframe(label="Bounding Box")],
+    [gr.Image(show_label=False), gr.Dataframe(label="Bounding Box", headers=["x0", "y0", "x1", "y1"])],
 )
 demo.launch()
